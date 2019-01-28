2c8523ffe3660b5df48d8e6b4c84e9a35f85041c
anoy <anoy@har.local>
2019-01-28 17:48:25 +0900

    1 Prob 1-1

--- src/Iterator/BookShelf.java
+++ src/Iterator/BookShelf.java
@@ -1,20 +1,20 @@
 package Iterator;
 
+import java.util.ArrayList;
+
 public class BookShelf implements Aggregate{
-    private Book[] books;
-    private int last = 0;
+    private ArrayList<Book> books;
     public BookShelf(int maxsize){
-        this.books = new Book[maxsize];
+        this.books = new ArrayList<Book>();
     }
     public Book getBookAt(int index){
-        return books[index];
+        return books.get(index);
     }
     public void appendBook(Book book){
-        this.books[last] = book;
-        last++;
+        books.add(book);
     }
     public int getLength(){
-        return last;
+        return books.size();
     }
     public Iterator iterator(){
         return new BookShelfIterator(this);
